9325bd3339d4613cff357afa4d37fca85a85b3e8
Tati92-L <77683219+Tati92-L@users.noreply.github.com>
2022-01-10 02:11:15 +0300

    Added greeting function

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync'
+
+const greeting = () => {
+const name = readlineSync.question('May I have your name?')
+ return console.log(`Hello, ${name}!`)
+}
+
+export default greeting;

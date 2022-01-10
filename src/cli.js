a32fdb077ee73a1de32e962350f1948a2de75c7e
Tati92-L <77683219+Tati92-L@users.noreply.github.com>
2022-01-10 17:32:07 +0300

    codeClimate and eslint and adding main.yml

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 const greeting = () => {
-const name = readlineSync.question('May I have your name?')
- return console.log(`Hello, ${name}!`)
-}
+  const name = readlineSync.question('May I have your name?');
+  return console.log(`Hello, ${name}!`);
+};
 
 export default greeting;

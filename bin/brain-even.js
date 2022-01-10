3eece97d9d749662981cf62367f0863cca11a62d
Tati92-L <77683219+Tati92-L@users.noreply.github.com>
2022-01-10 23:38:21 +0300

    added game and asciinema rec

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,34 +2,30 @@
 import readlineSync from 'readline-sync';
 
 const loop = (count, name) => {
-    for(let i = 0; i <= count; i++){
+  for (let i = 0; i <= count; i += 1) {
+    const num = Math.floor(Math.random() * 100);
+    console.log(`Question:, ${num}!`);
 
-        let num = Math.floor(Math.random() * 100)
-        console.log(`Question:, ${num}!`);
-
-        const answer = readlineSync.question('Your answer:')
-        let rightAnswer = num % 2 === 0 ? 'yes': 'no'
-        
-        if(rightAnswer !== answer){
-            console.log(`Let's try again, ${name}!`)
-            return loop(count, name)
-        }
-        console.log('Correct!')
+    const answer = readlineSync.question('Your answer:');
+    const rightAnswer = num % 2 === 0 ? 'yes' : 'no';
+    if (rightAnswer !== answer) {
+      console.log(`Let's try again, ${name}!`);
+      return loop(count, name);
     }
-    return console.log(`Congratulations, ${name}!`);
-}
-
-const evenOrnot = () => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${name}!`);
-
-    console.log ('Answer "yes" if the number is even, otherwise answer "no".')
-    let count = 2
-
-    return loop(count, name)
-
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${name}!`);
 };
 
-evenOrnot()
+const evenOrnot = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const count = 2;
+  return loop(count, name);
+};
+
+evenOrnot();
 export default evenOrnot;
